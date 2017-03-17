2639a78d39d1eb0e46700f7ba0d132dcd6a524dc
Ishani Mukherjee <ishanibhattacharjee@gmail.com>
2017-03-17 02:37:51 -0700

    checking first code examples of GraphQL

---
+++ 2-GraphQL-ServerExpress.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
+var { makeExecutableSchema } = require('graphql-tools');
+
+var typeDefs = [`
+
+type Car{
+	  name: String
+	  model: String
+	  make: Int
+	}
+	
+type Query {
+  hello: String
+  mycar: Car
+}
+
+schema {
+  query: Query
+}`];
+
+var resolvers = {
+  Query: {
+    hello(root) {
+      return 'world';
+    },
+	
+	mycar(root) {
+      return {
+		  name:"ABC",
+		  model:"Cool1989",
+		  make:65
+	  };
+    }
+  }
+};
+
+var schema = makeExecutableSchema({typeDefs, resolvers});
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
